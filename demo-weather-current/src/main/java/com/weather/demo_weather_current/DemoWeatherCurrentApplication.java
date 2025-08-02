cbf94886658204e32bc27a653bd71424c6e37e7e
Elise Wong <e.wong,hksarwork@gmail.com>
2025-08-02 15:57:03 +0800

    20250802

---
+++ demo-weather-current/src/main/java/com/weather/demo_weather_current/DemoWeatherCurrentApplication.java
@@ -0,0 +1,13 @@
+package com.weather.demo_weather_current;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeatherCurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeatherCurrentApplication.class, args);
+	}
+
+}
---
+++ demo-weather-current/src/test/java/com/weather/demo_weather_current/DemoWeatherCurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.demo_weather_current;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeatherCurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
